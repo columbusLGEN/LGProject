97378f535ebac83f26b6f905022ca553e5b32d18
INCUBUS <409362416@qq.com>
2018-01-05 17:25:12 +0800

    分类文件夹结构重构

---
+++ LGProject/LGProject/Public/Catagory/UICatagory/UIImage+Extension.h
@@ -0,0 +1,16 @@
+//
+//  UIImage+Extension.h
+//  LGProject
+//
+//  Created by Peanut Lee on 2018/1/5.
+//  Copyright © 2018年 LG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Extension)
+/** 改变UIImage 颜色 */
+- (UIImage *)changeImgColor:(UIColor *)color;
+/** 根据颜色生成 UIImage */
++ (UIImage *)imageWithColor:(UIColor *)color;
+@end
---
+++ LGProject/Pods/Target Support Files/Pods-LGProject/Pods-LGProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LGProject : NSObject
+@end
+@implementation PodsDummy_Pods_LGProject
+@end
