9f09c0d3b514fa1811acc34078a568994f4e80fe
INCUBUS <409362416@qq.com>
2018-04-12 09:29:30 +0800

    删除.svn目录

---
+++ proj/HBDJProj/HBDJProj/Public/Catagory/UICatagory/UIViewController+Extension.h
@@ -0,0 +1,14 @@
+//
+//  UIViewController+Extension.h
+//  HBDJProj
+//
+//  Created by Peanut Lee on 2018/4/12.
+//  Copyright © 2018年 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Extension)
+- (instancetype)lgInstantiateViewControllerWithStoryboardName:(NSString *)name controllerId:(NSString *)controllerId;
+- (void)lgPushViewControllerWithStoryboardName:(NSString *)name controllerId:(NSString *)controllerId animated:(BOOL)animated;
+@end
---
+++ proj/HBDJProj/HBDJProj/Public/Catagory/UICatagory/UIViewController+Extension.m
@@ -0,0 +1,22 @@
+//
+//  UIViewController+Extension.m
+//  HBDJProj
+//
+//  Created by Peanut Lee on 2018/4/12.
+//  Copyright © 2018年 Lee. All rights reserved.
+//
+
+#import "UIViewController+Extension.h"
+
+@implementation UIViewController (Extension)
+
+- (instancetype)lgInstantiateViewControllerWithStoryboardName:(NSString *)name controllerId:(NSString *)controllerId{
+    return [[UIStoryboard storyboardWithName:name bundle:nil] instantiateViewControllerWithIdentifier:controllerId];
+}
+
+- (void)lgPushViewControllerWithStoryboardName:(NSString *)name controllerId:(NSString *)controllerId animated:(BOOL)animated{
+    UIViewController *vc = [self lgInstantiateViewControllerWithStoryboardName:name controllerId:controllerId];
+    [self.navigationController pushViewController:vc animated:animated];
+}
+
+@end
