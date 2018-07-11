2a8f75ed27ac445e32c5f6faac6145a4d49a99d7
INCUBUS <409362416@qq.com>
2018-07-11 15:18:45 +0800

    修复配置文件异常

---
+++ proj/HBDJProj/HBDJProj/Classes/Online/OLMindReportModel.h
@@ -0,0 +1,15 @@
+//
+//  OLMindReportModel.h
+//  HBDJProj
+//
+//  Created by Peanut Lee on 2018/5/3.
+//  Copyright © 2018年 Lee. All rights reserved.
+//
+
+#import "DJThemeMeetingsModel.h"
+
+@interface OLMindReportModel : DJThemeMeetingsModel
+@property (copy,nonatomic) NSString *testTime;
+@property (copy,nonatomic) NSString *author;
+
+@end
