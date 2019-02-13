6673e32236915800188d9f42a6ab53be7f8364d7
columbusLGEN <murphior@163.com>
2019-02-13 18:45:39 +0800

    come back commit

---
+++ proj/HBDJProj/HBDJProj/Classes/Home/newsByWebvc/DJDNewsBrowserByWebController.h
@@ -0,0 +1,17 @@
+//
+//  DJDNewsBrowserByWebController.h
+//  HBDJProj
+//
+//  Created by lee on 2018/12/6.
+//  Copyright © 2018 Lee. All rights reserved.
+//
+
+#import "LGBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DJDNewsBrowserByWebController : LGBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ proj/HBDJProj/HBDJProj/Classes/Home/newsByWebvc/DJDNewsBrowserByWebController.m
@@ -0,0 +1,32 @@
+//
+//  DJDNewsBrowserByWebController.m
+//  HBDJProj
+//
+//  Created by lee on 2018/12/6.
+//  Copyright © 2018 Lee. All rights reserved.
+//
+
+#import "DJDNewsBrowserByWebController.h"
+
+@interface DJDNewsBrowserByWebController ()
+
+@end
+
+@implementation DJDNewsBrowserByWebController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
